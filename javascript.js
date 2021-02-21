658269d0f8e37bc081f422340bef30a4c9031789
Dgehring <dgehring7@gmail.com>
2021-02-21 13:56:56 -0500

    added the correct HTML and CSS files

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-!
+// 0a. load local storage
+// 0b. get time/display day
+// 0c. color timeblocks (past/future)
+// 1. user click timeblock
+//  1a. field to type in (forms)
+// 2. saves when save button clicked
+//   2a. onclick save-button
+//   2b. save in object property
+//   2c. store object in local storage
